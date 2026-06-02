b9a6a20202fd761c357a15c0b7664b2154cbe6ce
Kajanthuka <kajanthu18@gmail.com>
2026-06-02 14:49:34 +0000

    Styling file adding and cross origin allowd

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["192.168.40.118"],
   reactCompiler: true,
+
 };
 
 export default nextConfig;
