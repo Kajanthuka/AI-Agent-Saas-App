47b0441d770d9b5c590cc20de695229cdd34a2d9
Kajanthuka <kajanthu18@gmail.com>
2026-06-28 09:32:28 +0000

    Code clean

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,27 +1,3 @@
-// "use client";
-
-// import TopNav from "@/components/navbar/TopNav";
-// import SideNav from "@/components/navbar/SideNav";
-// import Footer from "@/components/footer/Footer";
-// import AuthGuard from "@/components/AuthGuard";
-
-// export default function AppShell({ children }: { children: React.ReactNode }) {
-//     return (
-//         <AuthGuard>
-//             <div className="min-h-screen bg-gray-50">
-//                 <TopNav />
-
-//                 <div className="flex pt-0">
-//                     <SideNav />
-//                     <main className="min-w-0 flex-1 p-6">{children}</main>
-//                 </div>
-
-//                 <Footer />
-//             </div>
-//         </AuthGuard>
-//     );
-// }
-
 "use client";
 
 import { usePathname } from "next/navigation";
